6e44c1fe3db6b2d86d145be64d57550c2542c0e8
Byonggon Chun <32089474+bg-chun@users.noreply.github.com>
2019-04-14 19:26:49 +0900

    Update gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -2,8 +2,9 @@
 
 int gcd_reculsive(int a, int b)
 {
+    int r = a%b;
     a = b;
-    b = a%b;
+    b = r;
     if ( b == 0)
     {
         return a;
@@ -16,8 +17,9 @@ int gcd_non_reculsive(int a, int b)
 {
     while(b != 0)
     {
+        int r = a%b;
         a = b;
-        b = a%b;
+        b = r;
     }
     
     return a;
@@ -27,10 +29,3 @@ int main()
 {
     int a = 64;
     int b = 12;
-    
-    std::cout << gcd_reculsive(a, b) << std::endl;
-    
-    std::cout << gcd_non_reculsive(a, b) << std::endl;
-    
-    return 0;
-}
